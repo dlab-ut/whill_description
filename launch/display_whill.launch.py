0d9feff40f98dbc845f504b445f5a061ad01a18d
yoshi-rob <95674429+yoshi-rob@users.noreply.github.com>
2024-09-20 03:38:02 +0900

    Initial commit

---
+++ launch/display_whill.launch.py
@@ -0,0 +1,30 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import os
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    urdf_file = os.path.join(
+        get_package_share_directory('whill_description'),
+        'urdf',
+        'whill.urdf')
+
+    rviz_config_file = os.path.join(
+        get_package_share_directory('whill_description'),
+        'rviz',
+        'whill_urdf.rviz')
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            arguments=[urdf_file],
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d', rviz_config_file]
+        ),
+    ])
